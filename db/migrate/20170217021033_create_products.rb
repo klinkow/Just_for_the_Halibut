c29d2555e683451a95b7440ff871ddbe4b654980
Bill Klinkow <klinkow@gmail.com>
2017-02-16 18:21:21 -0800

    first migration

---
+++ db/migrate/20170217021033_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.column :name, :string
+      t.column :description, :string
+      t.column :price, :float
+
+      t.timestamps
+    end
+  end
+end
